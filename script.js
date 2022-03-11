e6300ed6386e37e77cb1ab1d528dee5d8ac761eb
Hepheir <hepheir@gmail.com>
2022-03-11 14:31:31 +0900

    Fix second video not being skipped (refactor)

--- script.js
+++ script.js
@@ -6,44 +6,61 @@ function main() {
 class Bot {
     constructor(window) {
         this.window = window;
-        this.document = this.getContentFrame().contentDocument;
+        this.document = window.document;
 
+        // Properties
+        this.frame = null
+        this.video = null;
+
+        // Instance methods
         this.bindAutoPlay = this.bindAutoPlay.bind(this);
         this.bindAutoSkip = this.bindAutoSkip.bind(this);
-        this.getContentFrame = this.getContentFrame.bind(this);
+        this.getFrame = this.getFrame.bind(this);
         this.getPlayButton = this.getPlayButton.bind(this);
         this.getSkipButton = this.getSkipButton.bind(this);
         this.getVideo = this.getVideo.bind(this);
+        this.onFrameLoad = this.onFrameLoad.bind(this);
         this.onVideoEnded = this.onVideoEnded.bind(this);
         this.playVideo = this.playVideo.bind(this);
         this.skipVideo = this.skipVideo.bind(this);
         this.startAutoListening = this.startAutoListening.bind(this);
-    }
+        this.updateFrame = this.updateFrame.bind(this);
+        this.updateProperties = this.updateProperties.bind(this);
+        this.updateVideo = this.updateVideo.bind(this);
 
-    bindAutoSkip() {
-        console.log('자동 스킵 작업을 등록합니다.');
-        this.getVideo().onended = this.onVideoEnded;
+        this.updateProperties();
     }
 
     bindAutoPlay() {
         console.log('자동 재생 작업을 등록합니다.');
-        this.getContentFrame().onload = this.startAutoListening;
+        this.getFrame().onload = this.onFrameLoad;
     }
 
-    getContentFrame() {
-        return this.window.document.getElementById('contentWrap_content');
+    bindAutoSkip() {
+        console.log('자동 스킵 작업을 등록합니다.');
+        this.video.onended = this.onVideoEnded;
+    }
+
+    getFrame() {
+        return this.document.getElementById('contentWrap_content');
     }
 
     getPlayButton() {
-        return this.document.querySelector('#uPlayer > button');
+        return this.frame.contentDocument.querySelector('#uPlayer > button');
     }
 
     getSkipButton() {
-        return this.document.getElementById('next-btn');
+        return this.frame.contentDocument.getElementById('next-btn');
     }
 
     getVideo() {
-        return this.document.getElementById('uPlayer_html5_api');
+        return this.frame.contentDocument.getElementById('uPlayer_html5_api');
+    }
+
+    onFrameLoad() {
+        console.log('페이지가 갱신되었습니다.');
+        this.updateProperties();
+        this.startAutoListening();
     }
 
     onVideoEnded() {
@@ -53,7 +70,7 @@ class Bot {
 
     playVideo() {
         console.log('영상을 재생합니다.');
-        if (this.getVideo().paused)
+        if (this.video.paused)
             this.getPlayButton().click();
     }
 
@@ -64,9 +81,22 @@ class Bot {
 
     startAutoListening() {
         console.log('자동 수강을 시작합니다.');
-        this.bindAutoSkip();
         this.bindAutoPlay();
+        this.bindAutoSkip();
         this.playVideo();
+    }
+
+    updateFrame() {
+        this.frame = this.getFrame();
+    }
+
+    updateProperties() {
+        this.updateFrame();
+        this.updateVideo();
+    }
+
+    updateVideo() {
+        this.video = this.getVideo();
     }
 }
 
